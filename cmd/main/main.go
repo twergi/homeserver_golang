2de5b2f9fc35176fb983a93770a481399b9040df
Andrey Efimov <efimov.andrey.m@gmail.com>
2025-06-29 14:10:44 +0300

    add minecraft

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"html/template"
+	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 	"time"
 )
 
 var IP string = getLocalIP()
 var Port string = "80"
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/shutdown", shutdownHandler)
+	http.HandleFunc("/restart", restartHandler)
+	http.HandleFunc("/toggle_minecraft_server", toggleMinecraftServerHandler)
+	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer((http.Dir("static/css")))))
+
+	fullAddr := fmt.Sprintf("%s:%s", IP, Port)
+
+	println("starting server on:", fullAddr)
+	err := http.ListenAndServe(fullAddr, nil)
+
+	if err != nil {
+		panic(err)
+	}
+}
 
 func getLocalIP() string {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
@@ -79,22 +101,36 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	start := time.Date(2018, 1, 9, 18, 0, 0, 0, loc)
 	year, month, day, hour, min, sec := diff(start, time.Now())
 
+	_, minecraftServerRunning, err := getMincecraftServerPID()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	minectaftServerState := "✅"
+	if !minecraftServerRunning {
+		minectaftServerState = "❌"
+	}
+
 	tmpl.Execute(w, struct {
-		HostIP string
-		Year   int
-		Month  int
-		Day    int
-		Hour   int
-		Min    int
-		Sec    int
+		HostIP               string
+		Year                 int
+		Month                int
+		Day                  int
+		Hour                 int
+		Min                  int
+		Sec                  int
+		MinecraftServerState string
 	}{
-		HostIP: IP,
-		Year:   year,
-		Month:  month,
-		Day:    day,
-		Hour:   hour,
-		Min:    min,
-		Sec:    sec,
+		HostIP:               IP,
+		Year:                 year,
+		Month:                month,
+		Day:                  day,
+		Hour:                 hour,
+		Min:                  min,
+		Sec:                  sec,
+		MinecraftServerState: minectaftServerState,
 	})
 }
 
@@ -146,18 +182,95 @@ func restartHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/shutdown", shutdownHandler)
-	http.HandleFunc("/restart", restartHandler)
-	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer((http.Dir("static/css")))))
-
-	fullAddr := fmt.Sprintf("%s:%s", IP, Port)
-
-	println("starting server on:", fullAddr)
-	err := http.ListenAndServe(fullAddr, nil)
-
+func toggleMinecraftServerHandler(w http.ResponseWriter, r *http.Request) {
+	pid, ok, err := getMincecraftServerPID()
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
 	}
+
+	if ok {
+		err = killPID(pid)
+	} else {
+		err = startMinecraftServer("/home/twergi/projects/minecraft_server/paper-server.jar")
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+}
+
+func getMincecraftServerPID() (pid int, ok bool, err error) {
+	runnable := "ps -elf | grep -v grep | grep paper-server.jar"
+	cmd := exec.Command("bash", "-c", runnable)
+
+	var stdErr bytes.Buffer
+	cmd.Stderr = &stdErr
+
+	out, err := cmd.Output()
+	if err != nil {
+		ee, ok := err.(*exec.ExitError)
+		if !ok {
+			return 0, false, errors.Join(err, errors.New(stdErr.String()))
+		}
+
+		if ee.ExitCode() != 1 {
+			return 0, false, fmt.Errorf("got error, but exit code is not 1: %w", err)
+		}
+
+		return 0, false, nil
+	}
+
+	fields := strings.Fields(string(out))
+	if len(fields) < 4 {
+		return 0, false, errors.New("incorrect output, expected at least 4 elements: " + string(out))
+	}
+
+	pidStr := fields[3]
+	pid, err = strconv.Atoi(pidStr)
+	if err != nil {
+		return 0, false, fmt.Errorf("error parsing pid: %w", err)
+	}
+
+	return pid, true, nil
+}
+
+func killPID(pid int) error {
+	cmd := exec.Command("kill", strconv.Itoa(pid))
+	return runCmd(cmd)
+}
+
+func startMinecraftServer(serverPath string) error {
+	cmd := exec.Command("java", "-Xmx7G", "-Xms2G", "-jar", serverPath, "nogui")
+	return startCmd(cmd)
+}
+
+func runCmd(cmd *exec.Cmd) error {
+	var stdErr bytes.Buffer
+	cmd.Stderr = &stdErr
+
+	err := cmd.Run()
+	if err != nil {
+		return errors.Join(
+			err,
+			errors.New(stdErr.String()),
+		)
+	}
+
+	return nil
+}
+
+func startCmd(cmd *exec.Cmd) error {
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	log.Println("process started:", cmd.Process)
+
+	return nil
 }
